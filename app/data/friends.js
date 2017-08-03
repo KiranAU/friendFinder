44e5d1f66848fcf3c1c8fe9f0520d59f8b1d69e6
KiranAU <kiranuttarkar@Emilys-MacBook-Air.local>
2017-08-02 23:46:23 -0400

    pushing to git

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [{
+    "name": "Eric",
+    "photo": "https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2015/alberteinste.jpg",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Patrick",
+    "photo": "https://s-media-cache-ak0.pinimg.com/originals/56/fd/87/56fd87ce0696ccd1f40360cfdaed403b.png",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "Chandra",
+    "photo": "http://neilpatel.com/wp-content/uploads/2015/02/image19.jpg",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}, {
+    "name": "Chris",
+    "photo": "https://www.sideshowtoy.com/photo.php?sku=902622",
+    "scores": [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+}, {
+    "name": "Ali",
+    "photo": "https://vignette2.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest?cb=20121110131754",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "Dan",
+    "photo": "https://typeset-beta.imgix.net/2016/7/3/hp-cae3c33a-4904-4266-8f09-e5ef38f6a33b.jpg",
+    "scores": [4, 1, 5, 3, 4, 5, 2, 3, 4, 5]
+}, {
+    "name": "Cruz",
+    "photo": "https://vignette3.wikia.nocookie.net/superman/images/2/27/Superman-dcuo.jpg/revision/latest?cb=20110901025125",
+    "scores": [3, 4, 2, 4, 4, 1, 1, 2, 3, 4]
+}, {
+    "name": "Kiran",
+    "photo": "http://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png",
+    "scores": [5, 4, 2, 5, 1, 1, 2, 1, 3, 5]
+}, {
+    "name": "Hayden",
+    "photo": "http://i2.cdn.cnn.com/cnnnext/dam/assets/140224151731-mark-zuckerberg-story-top.jpg",
+    "scores": [1, 5, 1, 3, 5, 1, 4, 3, 2, 1]
+}, {
+    "name": "Maria",
+    "photo": "https://www.themarysue.com/wp-content/uploads/2017/04/IMG_0365.jpg",
+    "scores": [1, 3, 3, 1, 2, 4, 4, 1, 3, 1]
+}, {
+    "name": "Cody",
+    "photo": "https://vignette2.wikia.nocookie.net/nickelodeon/images/f/f2/JimmyNeutron.png/revision/latest?cb=20150411031453",
+    "scores": [5, 2, 4, 3, 3, 2, 1, 3, 3, 4]
+}, {
+    "name": "Tina",
+    "photo": "http://img10.deviantart.net/f47e/i/2016/038/9/b/superwoman_flexing_by_faeamara-d32sv7t.jpg",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+}, {
+    "name": "Jennifer",
+    "photo": "https://pbs.twimg.com/profile_images/725805903103037443/3GRX4TVP.jpg",
+    "scores": [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+}, {
+    "name": "Ricky",
+    "photo": "https://i.pinimg.com/236x/77/32/7f/77327f2f5bf8b1ebd39dc907a45a11c8--charles-michael-davis-celebrity-smiles.jpg",
+    "scores": [2, 3, 5, 5, 3, 5, 2, 1, 3, 2]
+}];
+
+module.exports = friends;
